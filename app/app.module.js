41724874991ab03879095e205fb18c6dd1531da5
kambehba <kamparsen@yahoo.com>
2015-12-17 16:24:48 -0500

    Initial check in for XOGame project

---
+++ app/app.module.js
@@ -0,0 +1,35 @@
+﻿(function () {
+    'use strict';
+
+    angular
+      .module('app', [
+        // Angular modules.
+        'ngRoute',
+
+        // Third party modules.
+        'firebase',
+
+        // Custom modules.
+        //'app.core',
+        //'app.project'
+      ])
+      .config(['$routeProvider', configFunction])
+      .run(runFunction, ['$rootScope', '$location']);
+
+    //configFunction.$inject = ['$routeProvider'];
+
+    function configFunction($routeProvider) {
+        $routeProvider.otherwise({
+            redirectTo: '/'
+        });
+    }
+
+    // runFunction.$inject = ['$rootScope', '$location'];
+
+    function runFunction($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function (event, next, previous, error) {
+
+        });
+    }
+
+})();
